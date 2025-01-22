7eedd89915c53641a5e691651b84226bd70a8504
Qwint <Qwint69@yandex.ru>
2025-01-22 11:02:48 +0300

    review

--- backend/src/films/schemas/film.schema.ts
+++ backend/src/films/schemas/film.schema.ts
@@ -1,47 +1,47 @@
-import { Schema, Document } from 'mongoose';
+// import { Schema, Document } from 'mongoose';
 
-export interface Schedule {
-  id: string;
-  daytime: string;
-  hall: number;
-  rows: number;
-  seats: number;
-  price: number;
-  taken: string[];
-}
+// export interface Schedule {
+//   id: string;
+//   daytime: string;
+//   hall: number;
+//   rows: number;
+//   seats: number;
+//   price: number;
+//   taken: string[];
+// }
 
-export interface Film extends Document {
-  id: string;
-  title: string;
-  rating: number;
-  director: string;
-  tags: string[];
-  image: string;
-  cover: string;
-  about: string;
-  description: string;
-  schedule: Schedule[];
-}
+// export interface Film extends Document {
+//   id: string;
+//   title: string;
+//   rating: number;
+//   director: string;
+//   tags: string[];
+//   image: string;
+//   cover: string;
+//   about: string;
+//   description: string;
+//   schedule: Schedule[];
+// }
 
-export const FilmSchema = new Schema<Film>({
-  id: { type: String, required: true, unique: true },
-  title: { type: String, required: true },
-  rating: { type: Number, required: true },
-  director: { type: String, required: true },
-  tags: { type: [String], required: true },
-  image: { type: String, required: true },
-  cover: { type: String, required: true },
-  about: { type: String, required: true },
-  description: { type: String, required: true },
-  schedule: [
-    {
-      id: { type: String, required: true },
-      daytime: { type: String, required: true },
-      hall: { type: Number, required: true },
-      rows: { type: Number, required: true },
-      seats: { type: Number, required: true },
-      price: { type: Number, required: true },
-      taken: { type: [String], required: true },
-    },
-  ],
-});
+// export const FilmSchema = new Schema<Film>({
+//   id: { type: String, required: true, unique: true },
+//   title: { type: String, required: true },
+//   rating: { type: Number, required: true },
+//   director: { type: String, required: true },
+//   tags: { type: [String], required: true },
+//   image: { type: String, required: true },
+//   cover: { type: String, required: true },
+//   about: { type: String, required: true },
+//   description: { type: String, required: true },
+//   schedule: [
+//     {
+//       id: { type: String, required: true },
+//       daytime: { type: String, required: true },
+//       hall: { type: Number, required: true },
+//       rows: { type: Number, required: true },
+//       seats: { type: Number, required: true },
+//       price: { type: Number, required: true },
+//       taken: { type: [String], required: true },
+//     },
+//   ],
+// });
